973939b8b0532f35560aafdd9543ddcc3890cb94
Aiko <59002254+aikobuny@users.noreply.github.com>
2023-08-27 15:17:31 +0800

    Create FE Shadow Script Hub.lua

---
+++ FE Shadow Script Hub.lua
@@ -0,0 +1,205 @@
+-- FE SHADOW SCRIPT HUB, ANIMATIONS, SUS SCRIPT NEKO V5, AND MORE!!
+wait(0.2)
+local function callback(text)
+    if text == "Yes" then
+	print("User said yes")
+    elseif text == "No" then
+	print("User said no")
+    end
+end
+
+local bindableFunction= Instance.new("BindableFunction")
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "DARKMODZ YOUTUBE";
+    Text = "Roblox FE Exploiting";
+    Duration = "2";
+    callbakc = bindableFunction;
+    Button1 = "OK";
+    
+})
+
+
+wait(0.1)
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Shadows Scripts HUB V2", "GrapeTheme")
+
+local Tab = Window:NewTab("| MAIN |")
+
+local Section = Tab:NewSection("random scripts ")
+
+Section:NewButton("FLY GUI V3 MODDED", "script owner: me_ozone modifier: shad0w_TYLER8", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/3UYg1qqb'))()
+end)
+
+Section:NewButton("Nameless animation V3 -My World (read info)", "keybinds: F R T Q | this script was not made by me", function()
+loadstring(game:HttpGet("https://pastebin.com/raw/CsKA3L8Z"))()
+end)
+
+Section:NewButton("OP | PRISON LIFE SCRIPT", "-when does prison life update? never....", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/VUzUCJGf'))()
+end)
+
+Section:NewButton("NUKER MOD GUI da hood(script owner in info)", "Made by tokixqp#3489 and Lucifear#0001", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/DDXCeVtk'))()
+end)
+
+Section:NewButton("KEYBOARD SCRIPT", "Made by Kokjindesu", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/YtdxEz0q'))()
+end)
+
+Section:NewButton("Doors script made by darkrai", "cool script for doors.... yeaahh", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/wm4eQSUQ'))()
+end)
+
+Section:NewButton("Infinite Yield FE", "MY FAVORITE SCRIPT", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/8fUy5TBM'))()
+end)
+
+Section:NewButton("OP | PRISON LIFE SCRIPT XtheMasterX", "SUPER OP", function()
+loadstring(game: HttpGet('https://raw.githubusercontent.com/XTheMasterX/Scripts/Main/PrisonLife'))()
+end)
+
+
+
+local Tab2 = Window:NewTab("| PLAYER |")
+
+local Section2 = Tab2:NewSection("Keyboards / cursor mobile ")
+
+Section2:NewSlider("Speed", "Set you speed", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+Section2:NewButton("FE | Giant script R15 (read info)", "keep your character's body proportions at maximum", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/2s98PrEE'))()
+end)
+
+Section2:NewButton("set Shift lock ", "only for mobile", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/792svEcC'))()
+end)
+
+Section2:NewButton("Telekinesis Script (need cursor script)", "a", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/Z33NcECh'))()
+end)
+
+
+local Tab3 = Window:NewTab("• R6 ANIM SCRIPTS •")
+
+local Section3 = Tab3:NewSection("R6 ANIMATIONS")
+
+Section3:NewButton("nameless animation V3", "BEST ANIMATION SCRIPT <3", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/CsKA3L8Z'))()
+end)
+
+Section3:NewButton("R6 ANIMATIONS GUI", "try nameless v3", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/vbJsvU0L'))()
+end)
+
+local Tab4 = Window:NewTab("• R15 ANIM SCRIPTS •")
+
+local Section4 = Tab4:NewSection("R15 ANIMATIONS")
+
+Section4:NewButton("R15 ANIMATIONS GUI + music gui", "R6 is better", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/tSmUTPXD'))()
+end)
+
+Section4:NewButton("FE R15 animations (read info)", "use Rthro animation to work", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/mbj17A7p'))()
+end)
+
+local Tab5 = Window:NewTab("• FLING SCRIPTS •")
+
+local Section5 = Tab5:NewSection("fling scripts")
+
+Section5:NewButton("FE CONTROLLER FLING (read info) ", "u need a backpack called 8-bit controller backpack", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/XRxfvKmR'))()
+end)
+
+Section5:NewButton("FE troll fling gui", "YEET!", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/XRxfvKmR'))()
+end)
+
+Section5:NewButton("FE ultimate fling gui", "fling your enemies!", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/r97d7dS0'))()
+end)
+
+Section5:NewButton("FE invisible fling (press Z to execute)", "fling your enemies!", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/B9G4qHGy'))()
+end)
+
+local Tab6 = Window:NewTab("DA HOOD")
+
+local Section6 = Tab6:NewSection("Da hood scripts ")
+
+Section6:NewButton("Unticled Hood V4.1", "BIGGEST GUI I HAVE EVER SEEN", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/JDeTFWrm'))()
+end)
+
+Section6:NewButton("Da hood VORTEX GUI", "this is cool", function()
+loadstring(game: HttpGet('https://raw.githubusercontent.com/ImagineProUser/vortexdahood/main/vortex'))()
+end)
+
+Section6:NewButton("NUKER MOD GUI da hood(script owner in info)", "Made by tokixqp#3489 and Lucifear#0001", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/DDXCeVtk'))()
+end)
+
+local Tab7 = Window:NewTab("PLS DONATE")
+
+local Section7 = Tab7:NewSection("Pls donate scripts ")
+
+Section7:NewButton("PLS DONATE GUI", "LETS GET SOME BEBUX BOIIII", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/jT3VNw1K'))()
+end)
+
+Section7:NewButton("Pls donate Fake donation", " idk ", function()
+loadstring(game: HttpGet('https://raw.githubusercontent.com/o5u3/PLS-Donate/main/Fake-Donate.lua'))()
+end)
+
+local Tab8 = Window:NewTab("Sus scripts")
+
+local Section8 = Tab8:NewSection("sus script | animations | nekos | +")
+
+
+Section8:NewButton("Pendulum Hub", " roblox thinks his game is for kids right? ", function()
+loadstring(game: HttpGet('https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Pendulum%20Hub%20V5.lua'))()
+end)
+
+Section8:NewButton("Sussy hub", " roblox thinks his game is for kids right? ", function()
+loadstring(game: HttpGet('https://gist.githubusercontent.com/Nilrogram/8b0c8bd710be142f383c71f79279752c/raw/e4fb01a7de7cd498bb53270d2ad191dfab268a88/FE%2520SussyHub'))()
+end)
+
+local Tab9 = Window:NewTab("EVADE")
+
+local Section9 = Tab9:NewSection("Evade scripts ")
+
+Section9:NewButton("Evade gui", "idk", function()
+loadstring(game: HttpGet('https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Evade/main.lua'))()
+end)
+
+Section9:NewButton("OP | Evade Gui (need Key)", "cool script but need Key :(", function()
+loadstring(game: HttpGet('https://pastebin.com/raw/uBvmEkWB'))()
+end)
+
+Section9:NewButton("OP | Evade Script GUI (need join discord server)", "WOW", function()
+loadstring(game: HttpGet('https://raw.githubusercontent.com/Robobo2022/script/main/Main.lua'))()
+end)
+
+Section9:NewButton("Darkrai-X Evade script", "", function()
+loadstring(game: HttpGet('https://raw.githubusercontent.com/GamingScripter/Darkrai-X/main/Games/Evade'))()
+end)
+
+
+
+local Tab10 = Window:NewTab("Keyboards")
+
+local Section10 = Tab10:NewSection("Keyboard | mobile cursor  ")
+
+Section10:NewButton("Keyboard", "made by Kokjindesu", function()
+loadstring(game: HttpGet('https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt'))()
+end)
+
+Section10:NewButton("mobile cursor", "mobile now have cursor haha", function()
+loadstring(game: HttpGet('https://pastefy.ga/V75mqzaz/raw'))()
+end)
